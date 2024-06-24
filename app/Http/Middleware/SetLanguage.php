d99f8508548f8f7775651cdd42a4cee7ad1b7dcd
Andrey Lomakin <Andry.lomakin1@gmail.com>
2024-06-24 14:12:05 +0300

    Баги

--- app/Http/Middleware/SetLanguage.php
+++ app/Http/Middleware/SetLanguage.php
@@ -5,84 +5,78 @@ namespace App\Http\Middleware;
 use App\Models\Setting;
 use Closure;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
 use PulkitJalan\GeoIP\Facades\GeoIP;
+use Jenssegers\Agent\Agent;
 use Request;
 
 class SetLanguage
 {
-
     public static function getLocale()
     {
-        // Получаем текущий URI запроса
         $uri = Request::path();
-
-        // Разбиваем URI на отдельные сегменты
         $segmentsURI = explode('/', $uri);
-        // Проверяем наличие метки языка среди доступных языков
+
         if (!empty($segmentsURI[0]) && in_array($segmentsURI[0], config('settings.languageCodes'))) {
-            // Если метка языка не является основным языком, возвращаем ее
             if ($segmentsURI[0] !== config('settings.mainLanguage')) {
                 return $segmentsURI[0];
             }
         }
 
-        // Если метка языка отсутствует или является основным языком, возвращаем null
         return null;
     }
 
     public static function getLocaleByCountryCode($countryCode)
     {
-        if(in_array(strtolower($countryCode), config('settings.languageCodes'))){
+        if (in_array(strtolower($countryCode), config('settings.languageCodes'))) {
             return strtolower($countryCode);
-        }else{
+        } else {
             return config('settings.mainLanguage');
         }
     }
 
-    /*
-    * Устанавливает язык приложения в зависимости от метки языка из URL
-    */
-
     public function handle($request, Closure $next)
     {
-        if(!isset($_SERVER['HTTP_HOST'])){
+        if (!isset($_SERVER['HTTP_HOST'])) {
             return $next($request);
         }
 
         $setting = Setting::getSettings($_SERVER['HTTP_HOST']);
-        $prefixCurrent = SetLanguage::getLocale();
+        $prefixCurrent = self::getLocale();
         $languages = config('settings.languages');
+
         if (!$prefixCurrent) {
             $prefixCurrent = config('settings.mainLanguage');
         }
 
-        // Если сессии нет
+        // Check if the request is from a bot
+        $agent = new Agent();
+        if ($agent->isRobot()) {
+            App::setLocale(self::getLanguageFromRequest($request->getRequestUri()));
+            return $next($request);
+        }
+
         if (!Session::has('language')) {
 
-            // Определяем настройку какой язык будем выводить
-            if($setting->determine_language_by == 'ip'){
+            if ($setting->determine_language_by == 'ip') {
                 $prefixByCountryUser = $this->getUserLanguageByIP($request->ip());
-            }elseif ($setting->determine_language_by == 'browser'){
+            } elseif ($setting->determine_language_by == 'browser') {
                 $prefixByCountryUser = $this->getUserLanguageByBrowser();
-            }else{
+            } else {
                 $prefixByCountryUser = $prefixCurrent;
             }
 
-
-            if ($prefixByCountryUser == $prefixCurrent) {
+            if ($prefixByCountryUser == $prefixCurrent || isset($languages[$prefixByCountryUser])) {
                 $language = $prefixByCountryUser;
-            }elseif (isset($languages[$prefixByCountryUser])) {
-                $language = $prefixByCountryUser;
-            }else{
+            } else {
                 $language = $prefixCurrent;
             }
 
             App::setLocale($language);
             $url = '/' . self::parseRequestUri($language, $request->getRequestUri());
             Session::put('language', $language);
-            if($url != $request->getRequestUri()){
+
+            if ($url != $request->getRequestUri()) {
                 return response()->redirectTo($url, 301);
             }
         } else {
@@ -93,7 +87,6 @@ class SetLanguage
         return $next($request);
     }
 
-    // Получаем язык пользователя по IP
     private function getUserLanguageByIP($ip)
     {
         $setip = GeoIP::setIp($ip);
@@ -101,7 +94,6 @@ class SetLanguage
         return $this->getLocaleByCountryCode($countryCode);
     }
 
-    // Получаем язык пользователя по языку в браузере
     private function getUserLanguageByBrowser()
     {
         $browserLanguages = $this->getBrowserLanguages();
@@ -113,12 +105,11 @@ class SetLanguage
                     return $settings['languages'][$lang]['code'];
                 }
             }
-        }else{
-            return config('settings.mainLanguage');
         }
+
+        return config('settings.mainLanguage');
     }
 
-// Получаем языки из браузера
     private function getBrowserLanguages()
     {
         $browserLanguages = [];
@@ -134,20 +125,32 @@ class SetLanguage
         return $browserLanguages;
     }
 
-    // Берем ссылку без домена и ищем языковый префикс, удаляем его и добавляем новый
+    public static function getLanguageFromRequest($url)
+    {
+        $array = explode('/', $url);
+
+        foreach ($array as $value){
+            if(in_array($value, config('settings.languageCodes'))){
+                return $value;
+            }
+        }
+
+        return config('settings.mainLanguage');
+    }
+
     public static function parseRequestUri($prefix, $url)
     {
         $array = explode('/', $url);
 
-        if($prefix != config('settings.mainLanguage')){
+        if ($prefix != config('settings.mainLanguage')) {
             $url = [$prefix];
-        }else{
+        } else {
             $url = [];
         }
 
-        if($array){
-            foreach ($array as $value){
-                if(!in_array($value, config('settings.languageCodes'))){
+        if ($array) {
+            foreach ($array as $value) {
+                if (!in_array($value, config('settings.languageCodes'))) {
                     $url[] = $value;
                 }
             }
