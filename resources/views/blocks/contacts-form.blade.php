433ccc87e109dd310bc14efffc4fd98e297126f1
Andrey Lomakin <Andry.lomakin1@gmail.com>
2024-06-26 18:00:01 +0300

    LAW-ECOMMERCEИ Иконки на стр. контакты (для мобайла -- под кнопкой связи)

--- resources/views/blocks/contacts-form.blade.php
+++ resources/views/blocks/contacts-form.blade.php
@@ -27,26 +27,7 @@
                         <button type="submit" class="form-contacts__submit--btn _icon-arrow-link">
                             {{ __('Подтвердить') }}
                         </button>
-                        <div class="socials">
-                            @if($settings->viber)
-                                <a href="viber://chat?number=%2B{{ $settings->viber }}" target="_blank"
-                                   class="socials__item socials__item--link _viber">
-                                    <img src="{{ asset('img/viber.svg') }}" alt="viber">
-                                </a>
-                            @endif
-                            @if($settings->whatsapp)
-                                <a href="https://wa.me/{{ $settings->whatsapp }}" target="_blank"
-                                   class="socials__item socials__item--link _whatsapp">
-                                    <img src="{{ asset('img/whatsapp.svg') }}" alt="whatsapp">
-                                </a>
-                            @endif
-                            @if($settings->telegram)
-                                <a href="{{ $settings->telegram }}" target="_blank"
-                                   class="socials__item socials__item--link _telegram">
-                                    <img src="{{ asset('img/telegram.svg') }}" alt="telegram">
-                                </a>
-                            @endif
-                        </div>
+                        @include('blocks.socials')
                     </div>
                     <input type="hidden" name="country_code" value="">
                     <input type="hidden" name="so" value="Invest">
